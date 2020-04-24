2e7cbb5030487af67b63dd0290757f089510cb47
arnavc1712 <arnav171296@gmail.com>
2020-04-23 23:56:29 -0700

    Added marker animation

--- components/destinationPicker.js
+++ components/destinationPicker.js
@@ -7,30 +7,16 @@ import configs from "../conf.json"
 navigator.geolocation = require('@react-native-community/geolocation')
 import MapView, { PROVIDER_GOOGLE,Marker,Callout,AnimatedRegion, Animated } from 'react-native-maps';
 import { Col, Row, Grid } from "react-native-easy-grid";
+import GetLocation from 'react-native-get-location'
 
 
-var getPosition = function () {
-    return new Promise(function (resolve, reject) {
-      navigator.geolocation.getCurrentPosition(resolve, reject);
-    });
-  }
-// animate(newCoordinate) {
-//     const { coordinate } = this.state;
-      
-//     if (this.map){
-//       this.map.animateToRegion({latitude: newCoordinate.latitude,
-//                  longitude: newCoordinate.longitude,
-//                  latitudeDelta: 0.015,
-//                  longitudeDelta: 0.0121,},1000)
-//     }
-//       if (Platform.OS === 'android') {
-//         if (this.marker) {
-//           this.marker._component.animateMarkerToCoordinate(newCoordinate, 1000);
-//         }
-//       } else {
-//         coordinate.timing(newCoordinate).start();
-//       }
-//     }
+// var getPosition = function () {
+//     return new Promise(function (resolve, reject) {
+//       navigator.geolocation.getCurrentPosition(resolve, reject,{enableHighAccuracy: true,timeout: 15000});
+//     });
+//   }
+
+
 
 const mapStyle = [
     {
@@ -49,59 +35,105 @@ const mapStyle = [
 
 const DestinationPicker = () => {
 
-    // const [selectedCoordinates, setSelectedCoordinates] = useState({latit})
+    const [currLocation, setCurrLocation] = useState({latitude:38.4214278,longitude:-111.9185674})
+    const [map,setMap] = useState()
+    const [marker,setMarker] = useState()
+    
+
     useEffect(() => {
         // Update the document title using the browser API
         const getCurrPos = async () => {
             try{
-                const location = await getPosition();
-                console.log(location)
+                const location = await GetLocation.getCurrentPosition({enableHighAccuracy: true,timeout: 15000,});
+                
+                let coordObj = {latitude:location["latitude"],longitude:location["longitude"]}
+                
+                setCurrLocation(coordObj)
+        
+                animate(coordObj);
             }
             catch(error){
-                console.log(error);
+                console.log(error)
             }
         }
         
         getCurrPos();
-      },[]);
+      },[map,marker]);
+    
+
+    const animate = (newCoordinates) => {
+ 
+        if (map){
+            map.animateToRegion({latitude: newCoordinates.latitude,
+                        longitude: newCoordinates.longitude,
+                        latitudeDelta: 0.015,
+                        longitudeDelta: 0.0121,},1000)
+        }
+
+        if (Platform.OS === 'android') {
+            if (marker) {
+        
+                marker["animateMarkerToCoordinate"](newCoordinates, 1000);
+            }
+            } else {
+                currLocation.timing(newCoordinates).start();
+            }
+        }
    
     return (<Grid>
         <Row size={5} >
             <MapView
-                // showUserLocation={true}
-                ref={(map) => { map = map; }}
+                showUserLocation={true}
+                ref={(map) => { setMap(map); }}
                 customMapStyle={mapStyle}
                 provider={PROVIDER_GOOGLE} // remove if not using Google Maps
                 style={styles.map}
                 initialRegion={{
-                latitude: 33.419734999999996,
-                longitude: -111.9164663,
+                latitude: currLocation["latitude"],
+                longitude: currLocation["longitude"],
                 latitudeDelta: 0.015,
                 longitudeDelta: 0.0121,
             }}
             >
-               
+            
+            <Marker.Animated
+                ref={mark => { setMarker(mark); }}
+                style={{zIndex:1}}
+                flat={true}
+                coordinate={currLocation}
+            />    
             
             </MapView>
 
              <GooglePlacesAutocomplete
-            placeholder='Enter Location'
-            minLength={2}
-            autoFocus={true}
-            returnKeyType={'default'}
-            fetchDetails={true}
-            onPress={(data, details = null) => { // 'details' is provided when fetchDetails = true
-            console.log(data, details);
+                placeholder='Enter Location'
+                minLength={2}
+                autoFocus={false}
+                listViewDisplayed={false}
+                returnKeyType={'default'}
+                fetchDetails={true}
+                onPress={(data, details = null) => { // 'details' is provided when fetchDetails = true
+                    let name = null;
+                    if ("description" in data){
+                        name = data["description"].split(",")[0]
+                    }
+                    else{
+                        name = data["name"].split(",")[0]
+                    }
+                    let coordObj = {latitude:details["geometry"]["location"]["lat"],longitude:details["geometry"]["location"]["lng"]};
+                    animate(coordObj);
+                    setCurrLocation(coordObj);
+                    
                 }}
-            styles={autocompleteSyles}
-            currentLocation={true}
-            currentLocationLabel="Places around me"
-            query={{
-                // available options: https://developers.google.com/places/web-service/autocomplete
-                key: configs["mapsDirectionsKey"],
-                language: 'en', // language of the results
-                // types: '(cities)' // default: 'geocode'
-            }}
+                styles={autocompleteSyles}
+                currentLocation={true}
+                currentLocationLabel="Places around me"
+                query={{
+                    // available options: https://developers.google.com/places/web-service/autocomplete
+                    key: configs["mapsDirectionsKey"],
+                    language: 'en', // language of the results
+                    // types: '(cities)' // default: 'geocode'
+                }}
             />
           </Row>
     </Grid>)

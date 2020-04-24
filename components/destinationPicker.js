64cf09332096ca2a349064f2d38826dcc3c704ec
arnavc1712 <arnav171296@gmail.com>
2020-04-23 21:41:43 -0700

    Added geolocation

--- components/destinationPicker.js
+++ components/destinationPicker.js
@@ -1,18 +1,19 @@
-import React, {Component,useState} from 'react';
+import React, {Component,useState, useEffect} from 'react';
 import {StyleSheet} from 'react-native';
 import {Container} from 'native-base'
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import configs from "../conf.json"
+// import Geolocation from '@react-native-community/geolocation';
 navigator.geolocation = require('@react-native-community/geolocation')
 import MapView, { PROVIDER_GOOGLE,Marker,Callout,AnimatedRegion, Animated } from 'react-native-maps';
 import { Col, Row, Grid } from "react-native-easy-grid";
-// navigator.geolocation.getCurrentPosition(
-//     (position) => {
-//         console.log(position);
-//     },
-//     (error) => console.log(new Date(), error),
-//     {enableHighAccuracy: true, timeout: 10000, maximumAge: 3000}
-// );  
+
+
+var getPosition = function () {
+    return new Promise(function (resolve, reject) {
+      navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+  }
 // animate(newCoordinate) {
 //     const { coordinate } = this.state;
       
@@ -47,28 +48,25 @@ const mapStyle = [
 ]
 
 const DestinationPicker = () => {
+
+    // const [selectedCoordinates, setSelectedCoordinates] = useState({latit})
+    useEffect(() => {
+        // Update the document title using the browser API
+        const getCurrPos = async () => {
+            try{
+                const location = await getPosition();
+                console.log(location)
+            }
+            catch(error){
+                console.log(error);
+            }
+        }
+        
+        getCurrPos();
+      },[]);
+   
     return (<Grid>
-        <Row size={2} style={{zIndex:9999}}>
-            <GooglePlacesAutocomplete
-            placeholder='Enter Location'
-            minLength={2}
-            autoFocus={false}
-            returnKeyType={'default'}
-            fetchDetails={true}
-            onPress={(data, details = null) => { // 'details' is provided when fetchDetails = true
-            console.log(data, details);
-                }}
-            styles={autocompleteSyles}
-            currentLocation={true}
-            query={{
-                // available options: https://developers.google.com/places/web-service/autocomplete
-                key: configs["mapsDirectionsKey"],
-                language: 'en', // language of the results
-                // types: '(cities)' // default: 'geocode'
-            }}
-            />
-        </Row>
-        <Row size={5} style={{zIndex:-1}}>
+        <Row size={5} >
             <MapView
                 // showUserLocation={true}
                 ref={(map) => { map = map; }}
@@ -81,20 +79,43 @@ const DestinationPicker = () => {
                 latitudeDelta: 0.015,
                 longitudeDelta: 0.0121,
             }}
-                
             >
+               
             
             </MapView>
+
+             <GooglePlacesAutocomplete
+            placeholder='Enter Location'
+            minLength={2}
+            autoFocus={true}
+            returnKeyType={'default'}
+            fetchDetails={true}
+            onPress={(data, details = null) => { // 'details' is provided when fetchDetails = true
+            console.log(data, details);
+                }}
+            styles={autocompleteSyles}
+            currentLocation={true}
+            currentLocationLabel="Places around me"
+            query={{
+                // available options: https://developers.google.com/places/web-service/autocomplete
+                key: configs["mapsDirectionsKey"],
+                language: 'en', // language of the results
+                // types: '(cities)' // default: 'geocode'
+            }}
+            />
           </Row>
     </Grid>)
 }
 
 const autocompleteSyles = {
+    listView:{backgroundColor:'white',maxHeight:'30%'},
     
+    // position:'absolute',
     textInputContainer: {
     backgroundColor: 'rgba(0,0,0,0)',
     borderTopWidth: 0,
     borderBottomWidth:0,
+    // position:'absolute'
     },
     textInput: {
     marginLeft: 0,
@@ -102,9 +123,10 @@ const autocompleteSyles = {
     height: 38,
     color: '#5d5d5d',
     fontSize: 16,
+    // position:'absolute'
     },
     predefinedPlacesDescription: {
-    color: '#1faadb',
+        color: '#1faadb',
     },
 }
 
@@ -117,7 +139,7 @@ const styles = StyleSheet.create({
     },
     map: {
       ...StyleSheet.absoluteFillObject,
-   
+        position:'absolute',
     },
   
    });

4b66bcad81e68d12bab51e50f4e05c892ecd729e
Abhishek Chaudhary <abhidagar@rocketmail.com>
2020-04-24 23:28:36 -0700

    Added function to get waypoints info

--- components/destinationPicker.js
+++ components/destinationPicker.js
@@ -9,6 +9,7 @@ import MapView, { PROVIDER_GOOGLE,Marker,Callout,AnimatedRegion, Animated } from
 import { Col, Row, Grid } from "react-native-easy-grid";
 import GetLocation from 'react-native-get-location';
 import { Text ,Button,Content,Form,Input,Root,Header,Body,Title,Item as FormItem,Label,Toast} from 'native-base';
+import {GetPaths} from './getPaths';
 
 
 // var getPosition = function () {
@@ -46,14 +47,15 @@ const DestinationPicker = ({distance, currLocation,setCurrLocation,startLocation
             try{
                 const location = await GetLocation.getCurrentPosition({enableHighAccuracy: true,timeout: 15000,});
                 let coordObj = {latitude:location["latitude"],longitude:location["longitude"]}
-                setstartLocation(coordObj);
-                //Start location if set
-                //Get distance as props
-                console.log(coordObj);
+                setStartLocation(coordObj);
                 setCurrLocation(coordObj)
                 animate(coordObj);
-                console.log(location);
-                getEndPoints(coordObj);
+
+                // await getEndPoints(coordObj);
+                // getEndPoints(coordObj);
+                // const origin = {latitude: 33.419734999999996, longitude: -111.9164663};
+                GetPaths(coordObj, '5K');
+                
             }
             catch(error){
                 console.log("Error 1", error)
@@ -61,37 +63,10 @@ const DestinationPicker = ({distance, currLocation,setCurrLocation,startLocation
         }
         
         getCurrPos();
-        
       },[map,marker]);
     
 
-    const getEndPoints = async (coordObj) => {
-        let URL = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${coordObj['latitude']},${coordObj['longitude']}&radius=${distance.replace('K', '000')}&key=AIzaSyBMV8iHwK-7GW_xUtoBn0uICBvJsrXeXJI`
-        try{
-            await fetch(URL)
-            .then((response) => {
-                return response.json();
-            })
-            .then((data) => {
-                
-                let marker_info = [];
-                data['results'].forEach(element => {
-                    let lat = element.geometry.location.lat;
-                    let lng = element.geometry.location.lng;
-                    let name = element.name;
-                    let icon = element.icon;
-                    marker_info.push({'lat':lat, 'lng':lng, 'name':name, 'icon':icon});
-                });
-                // console.log(marker_info);
-                setDestinationMarkers(marker_info);
-            });
-            
-        }
-        catch(error){
-            console.log("Error 2", error)
-            Toast.show({text:error.toString().split("Error:")[1].trim(),buttonText:"Okay",duration:3000})
-        }
-    }  
+
 
     const animate = (newCoordinates) => {
         if (map){
@@ -102,14 +77,16 @@ const DestinationPicker = ({distance, currLocation,setCurrLocation,startLocation
         }
         if (Platform.OS === 'android') {
             if (marker) {
-                marker["animateMarkerToCoordinate"](newCoordinates, 1000);
+                marker["animateMarkerToCoordinate"](newCoordinates, 10);
             }
             } else {
                 startLocation.timing(newCoordinates).start();
             }
         }
-   
-    return (<Grid>
+        
+    return (
+    
+    <Grid>
         <Row size={5} >
             <MapView
                 showUserLocation={true}
@@ -118,33 +95,32 @@ const DestinationPicker = ({distance, currLocation,setCurrLocation,startLocation
                 provider={PROVIDER_GOOGLE} // remove if not using Google Maps
                 style={styles.map}
                 initialRegion={{
-                latitude: startLocation["latitude"],
-                longitude: startLocation["longitude"],
-                latitudeDelta: 0.015,
-                longitudeDelta: 0.0121,
-            }}
+                    latitude: currLocation["latitude"],
+                    longitude: currLocation["longitude"],
+                    latitudeDelta: 0.015,
+                    longitudeDelta: 0.0121,
+                }}
             >
             <Marker.Animated
                 ref={mark => { setMarker(mark); }}
                 style={{zIndex:1}}
                 flat={true}
-                coordinate={startLocation}
-                key="startLocationMarker"
-            />    
-            
-            {destinationMarker != null ? destinationMarker.map((item, index) =>(
+                coordinate={currLocation}
+                key="Asasas"
+            />     
+            {/* {destinationMarker != null && destinationMarker.length > 0? destinationMarker.map((item, index) =>(
                 <MapView.Marker 
                 coordinate={{'latitude':item.lat, 'longitude':item.lng}} 
                 title={item.name} 
                 icon={item.icon} 
                 key={index}
                 />
-            )) : <Text></Text>}
+            )) : <Text></Text>} */}
 
 
             </MapView>
 
-             <GooglePlacesAutocomplete
+            <GooglePlacesAutocomplete
                 placeholder='Enter Location'
                 minLength={2}
                 autoFocus={false}
@@ -161,7 +137,8 @@ const DestinationPicker = ({distance, currLocation,setCurrLocation,startLocation
                     }
                     let coordObj = {latitude:details["geometry"]["location"]["lat"],longitude:details["geometry"]["location"]["lng"]};
                     animate(coordObj);
-                    setstartLocation(coordObj);
+                    setCurrLocation(coordObj);
+                    GetPaths(coordObj, '5K');
                     
                 }}
                 styles={autocompleteSyles}
